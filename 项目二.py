1430229c8694ccac877f63600ade70f375755cbc
wuxueyao1999 <42131274+wuxueyao1999@users.noreply.github.com>
2018-08-06 18:10:26 +0800

    Add files via upload
    遗传算法的基本原理：将问题模拟成一个生物进化过程，通过遗传，交叉，突变，自然选择等操作产生下一代的解，并逐步淘汰适应度函数值低的解，增加适应度函数高的解。这样进化N代后就很有可能会进化出适应度函数值很高的个体，即所要求的值。

---
+++ 项目二.py
@@ -0,0 +1,77 @@
+
+# coding: utf-8
+
+# In[8]:
+
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Find the maximum for function: f(x) =-3*(x-30)**2*sin(x) 
+'''
+import matplotlib.pyplot as plt
+import numpy as np
+from math import sin, cos
+from gaft import GAEngine
+from gaft.components import BinaryIndividual
+from gaft.components import Population
+from gaft.operators import TournamentSelection
+from gaft.operators import UniformCrossover
+from gaft.operators import FlipBitMutation
+
+x = np.linspace(0, 15, 256)
+y= -3*(x-30)**2*np.sin(x) 
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.plot(x, y)
+
+
+# Analysis plugin base class.
+from gaft.plugin_interfaces.analysis import OnTheFlyAnalysis
+
+# Built-in best fitness analysis.
+from gaft.analysis.fitness_store import FitnessStore
+
+# Define population.
+indv_template = BinaryIndividual(ranges=[(0, 15)], eps=0.001)
+population = Population(indv_template=indv_template, size=30).init()
+
+# Create genetic operators.
+selection = TournamentSelection()
+crossover = UniformCrossover(pc=0.8, pe=0.5)
+mutation = FlipBitMutation(pm=0.1)
+
+# Create genetic algorithm engine.
+engine = GAEngine(population=population, selection=selection,
+                  crossover=crossover, mutation=mutation,
+                  analysis=[FitnessStore])
+
+# Define fitness function.
+@engine.fitness_register
+def fitness(indv):
+    x, = indv.solution
+    return -3*(x-30)**2*sin(x)
+
+# Define on-the-fly analysis.
+@engine.analysis_register
+class ConsoleOutputAnalysis(OnTheFlyAnalysis):
+    interval = 1
+    master_only = True
+
+    def register_step(self, g, population, engine):
+        best_indv = population.best_indv(engine.fitness)
+        msg = 'Generation: {}, best fitness: {:.3f}'.format(g, engine.ori_fmax)
+        self.logger.info(msg)
+
+    def finalize(self, population, engine):
+        best_indv = population.best_indv(engine.fitness)
+        x = best_indv.solution
+        y = engine.ori_fmax
+        msg = 'Optimal solution: ({}, {})'.format(x, y)
+        self.logger.info(msg)
+
+if '__main__' == __name__:
+    # Run the GA engine.
+    engine.run(ng=100)
+
