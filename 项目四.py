8987565cbedeca17e3b08e337b056d699126c91d
wuxueyao1999 <42131274+wuxueyao1999@users.noreply.github.com>
2018-08-10 09:03:56 +0800

    Update 项目四.py

--- 项目四.py
+++ 项目四.py
@@ -8,8 +8,8 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
-x_data = np.linspace(-1,1,300)[:, np.newaxis]
-noise = np.random.normal(0, 0.05, x_data.shape)
+x_data = np.linspace(-4,4,300)[:, np.newaxis]
+noise = np.random.normal(0, 0.5, x_data.shape)
 y_data = np.square(x_data)+1*np.random.random()+ noise-25
 
 plt.plot(x_data, y_data)
@@ -17,12 +17,12 @@ plt.plot(x_data, y_data)
 xs = tf.placeholder(tf.float32, [None, 1])
 ys = tf.placeholder(tf.float32, [None, 1])
 
-w1 = tf.Variable(tf.random_normal([1, 35]))
-b1 = tf.Variable(tf.zeros([1, 35]) + 0.1)
+w1 = tf.Variable(tf.random_normal([1, 10]))
+b1 = tf.Variable(tf.zeros([1, 10]) + 0.1)
 ip1 = tf.matmul(xs, w1) + b1
-out1 = tf.nn.relu(ip1)
+out1 = tf.nn.sigmoid(ip1)
 
-w2 = tf.Variable(tf.random_normal([35,1]))
+w2 = tf.Variable(tf.random_normal([10,1]))
 b2 = tf.Variable(tf.zeros([1, 1]) +0.1)
 ip2 = tf.matmul(out1, w2) + b2
 out2 = ip2
